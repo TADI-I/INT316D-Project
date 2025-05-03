2dc5aa441504e0df3c5df8f7eb1eff5b9c21f5eb
Tadiwanashe Songore <119779084+TADI-I@users.noreply.github.com>
2025-05-03 19:21:53 +0200

    Entity class created and Added as a JAR

---
+++ ToDoListEJB/src/java/entiies/TasksFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entiies;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Student
+ */
+@Stateless
+public class TasksFacade extends AbstractFacade<Tasks> implements TasksFacadeLocal {
+
+    @PersistenceContext(unitName = "ToDoListEJBPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public TasksFacade() {
+        super(Tasks.class);
+    }
+    
+}
---
+++ ToDoListEJB/src/java/entiies/TasksFacadeLocal.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entiies;
+
+import java.util.List;
+import javax.ejb.Local;
+
+/**
+ *
+ * @author Student
+ */
+@Local
+public interface TasksFacadeLocal {
+
+    void create(Tasks tasks);
+
+    void edit(Tasks tasks);
+
+    void remove(Tasks tasks);
+
+    Tasks find(Object id);
+
+    List<Tasks> findAll();
+
+    List<Tasks> findRange(int[] range);
+
+    int count();
+    
+}
