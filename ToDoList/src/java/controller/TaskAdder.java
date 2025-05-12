00be9f56ee49db6d5d1ed9049b17a35c46140187
Ashley <ashleymanchidi@gmail.com>
2025-05-12 22:13:06 +0200

    Add folder to new-branch

--- ToDoList/src/java/controller/TaskAdder.java
+++ ToDoList/src/java/controller/TaskAdder.java
@@ -5,8 +5,14 @@
  */
 package controller;
 
+import entities.Tasks;
+import entities.TasksFacadeLocal;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
+import javafx.concurrent.Task;
+import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,72 +23,25 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Student
  */
-@WebServlet(name = "TaskAdde", urlPatterns = {"/TaskAdde"})
+@WebServlet(name = "TaskAdder", urlPatterns = {"/TaskAdder"})
 public class TaskAdder extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet TaskAdder</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet TaskAdder at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    
+    @EJB
+    private TasksFacadeLocal tfl;
+    
+    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        
+        String title = request.getParameter("title");
+        String desc = request.getParameter("description");
+        
+        Tasks task = new Tasks(title, desc, "Incomplete", new Date());
+        tfl.create(task);
+        
+        RequestDispatcher rd = request.getRequestDispatcher("successfully-added.html");
+        rd.forward(request, response);
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
